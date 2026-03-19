92807cd11ec0f3cd47f293c781f294abf94896ba
Amit Kumar <ramit5752@gmail.com>
2026-03-20 01:26:59 +0530

    follow and like functionality add

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
-const authRouter = require("./routes/auth.route")
-const postRouter = require("./routes/post.routes")
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
 
+const authRouter = require("./routes/auth.route")
+const postRouter = require("./routes/post.routes")
+const userRouter = require("./routes/user.route")
+
 
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
+app.use("/api/users", userRouter)
 
 module.exports = app
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -31,7 +31,8 @@ const registerController = async (req,res)=>{
     })
 
     const token = jwt.sign({
-        id: user._id
+        id: user._id,
+        username: user.username
     },process.env.JWT_SECRET, {expiresIn: "1d"})
 
     res.cookie("token", token)
@@ -80,7 +81,8 @@ const loginController = async (req,res)=>{
     }
 
     const token = jwt.sign({
-        id: user._id
+        id: user._id,
+        username: user.username
     }, process.env.JWT_SECRET, {expiresIn: "1d"})
 
     res.cookie("token", token)
